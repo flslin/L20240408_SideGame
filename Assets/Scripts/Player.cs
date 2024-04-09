fa41510866a375cdce90d06367875e79ec2d831e
flslin <84002788+flslin@users.noreply.github.com>
2024-04-09 10:49:32 +0900

    플레이어 이동 플레이어 점프 제한

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public Animator anim;
+
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpForce;
+
+    private float xInput;
+    private int facingDir = 1;
+    private bool facingRight = false;
+
+    [Header("Collision info")]
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] private LayerMask whatIsGround;
+    private bool isGround;
+    //private bool isMoving; // 함수 내에서만 사용하므로 지역으로 선언
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 캐싱
+        rb = GetComponent<Rigidbody2D>();
+        //anim = GetComponentInChildren<Animator>(); // 자식 컴포넌트를 가져오는것
+        //rb.velocity = new Vector2 (5, rb.velocity.y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement(); // 움직임
+
+        CheckInput(); // 입력 체크
+
+        CollisionChecks(); // 땅에 닿았는지 체크
+
+        FlipController(); // 방향 회전
+
+        AinmatorController(); // 애니메이션
+    }
+
+    private void CollisionChecks()
+    {
+        isGround = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, whatIsGround);
+    }
+
+    private void CheckInput()
+    {
+        xInput = Input.GetAxis("Horizontal");
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Jump");
+            Jump();
+        }
+    }
+
+    private void Movement()
+    {
+        rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y);
+    }
+
+    private void Jump()
+    {
+        if(isGround)
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+    }
+
+    private void AinmatorController()
+    {
+        bool isMoving = rb.velocity.x != 0; // 0이 아닐경우 true
+        anim.SetBool("isMoving", isMoving);
+    }
+
+    private void Flip()
+    {
+        facingDir = facingDir * -1;
+        facingRight = !facingRight;
+        transform.Rotate(0, 180, 0);
+    }
+
+    private void FlipController()
+    {
+        if (rb.velocity.x < 0 && !facingRight)
+        {
+            Flip();
+        }
+        else if (rb.velocity.x > 0 && facingRight)
+        {
+            Flip();
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, new Vector3(transform.position.x, transform.position.y - groundCheckDistance));
+    }
+}
