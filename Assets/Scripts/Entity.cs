314b08c06a96e2aad3b23b5e237786bc724aa588
flslin <84002788+flslin@users.noreply.github.com>
2024-04-11 16:17:13 +0900

    적 구현

--- Assets/Scripts/Entity.cs
+++ Assets/Scripts/Entity.cs
@@ -11,7 +11,10 @@ public class Entity : MonoBehaviour
     [SerializeField] protected Transform groundCheck;
     [SerializeField] protected float groundCheckDistance;
     [SerializeField] protected LayerMask whatIsGround;
+    [SerializeField] protected Transform wallCheck;
+    [SerializeField] protected float wallCheckDistance;
     protected bool isGround;
+    protected bool isWall;
 
     protected int facingDir = 1;
     protected bool facingRight = false;
@@ -21,6 +24,11 @@ public class Entity : MonoBehaviour
     {
         anim = GetComponentInChildren<Animator>();
         rb = GetComponent<Rigidbody2D>();
+
+        if (wallCheck != null)
+        {
+            wallCheck = transform;
+        }
     }
 
     // Update is called once per frame
@@ -32,6 +40,7 @@ public class Entity : MonoBehaviour
     protected virtual void CollisionChecks()
     {
         isGround = Physics2D.Raycast(groundCheck.position, Vector2.down, groundCheckDistance, whatIsGround);
+        isWall = Physics2D.Raycast(wallCheck.position, Vector2.right, wallCheckDistance * facingDir, whatIsGround);
     }
 
     protected virtual void Flip()
@@ -44,5 +53,6 @@ public class Entity : MonoBehaviour
     protected virtual void OnDrawGizmos()
     {
         Gizmos.DrawLine(groundCheck.position, new Vector3(groundCheck.position.x, groundCheck.position.y - groundCheckDistance));
+        Gizmos.DrawLine(wallCheck.position, new Vector3(wallCheck.position.x + wallCheckDistance * facingDir, wallCheck.position.y));
     }
 }
