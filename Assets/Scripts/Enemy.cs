a12afd98a649e15188ae142bff5390d39983317a
flslin <84002788+flslin@users.noreply.github.com>
2024-04-12 09:35:32 +0900

    플레이어 감지

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class Enemy : Entity
 {
+    bool isAttack;
+
     [Header("Move info")]
     [SerializeField] private float moveSpeed;
+    
+    [Header("Player detection")]
+    [SerializeField] private float playerCheckDistance;
+    [SerializeField] private LayerMask whatIsPlayer;
+
+    private RaycastHit2D isPlayerDetection;
 
     protected override void Start()
     {
@@ -16,11 +24,48 @@ public class Enemy : Entity
     {
         base.Update();
 
-        if(!isGround || isWall)
+        if (isPlayerDetection)
+        {
+            if (isPlayerDetection.distance > 1)
+            {
+                rb.velocity = new Vector2(moveSpeed * facingDir, rb.velocity.y);
+
+                Debug.Log("플레이어 찾기");
+                isAttack = false;
+            }
+            else
+            {
+                Debug.Log("공격" + isPlayerDetection);
+                isAttack = true;
+            }
+        }
+
+        if (!isGround || isWall)
         {
             Flip();
         }
 
-        rb.velocity = new Vector2(moveSpeed * facingDir, rb.velocity.y);
+        Movement();
+    }
+
+    private void Movement()
+    {
+        if (!isAttack)
+            rb.velocity = new Vector2(moveSpeed * facingDir, rb.velocity.y);
+    }
+
+    protected override void CollisionChecks()
+    {
+        base.CollisionChecks();
+
+        isPlayerDetection = Physics2D.Raycast(transform.position, Vector2.right, playerCheckDistance * facingDir, whatIsPlayer);
+    }
+
+    protected override void OnDrawGizmos()
+    {
+        base.OnDrawGizmos();
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(transform.position, new Vector3(transform.position.x + playerCheckDistance * facingDir, transform.position.y));
     }
 }
